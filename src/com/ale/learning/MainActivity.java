230e4c4b114306a17af2552a5bf436397bbe93a0
WeimaSoft <Alexandersu@126.com>
2016-09-12 14:31:15 +0800

    Add multiple buttons dialog

--- src/com/ale/learning/MainActivity.java
+++ src/com/ale/learning/MainActivity.java
@@ -17,7 +17,45 @@ public class MainActivity extends Activity {
 
 		Button btnOKCancel = (Button) findViewById(R.id.btnOKCancel);
 		btnOKCancel.setOnClickListener(okCancelListner);
+
+		Button btnMultiBtns = (Button) findViewById(R.id.btnMultipleButtons);
+		btnMultiBtns.setOnClickListener(multiButtonsListner);
 	}
+
+	private OnClickListener multiButtonsListner = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+			builder.setTitle("你确定要离开吗？");
+			builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					showDialog("你选择了确定");
+
+				}
+			});
+
+			builder.setNeutralButton("其实我也不知道", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					showDialog("没想到你也不知道");
+
+				}
+			});
+
+			builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					showDialog("你选择了取消");
+
+				}
+			});
+			builder.create().show();
+		}
+	};
 
 	private OnClickListener okCancelListner = new OnClickListener() {
 		@Override
@@ -28,19 +66,27 @@ public class MainActivity extends Activity {
 
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
+					showDialog("你选择了确定");
 
 				}
 			});
+
 			builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
 
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
+					showDialog("你选择了取消");
 
 				}
 			});
 			builder.create().show();
 		}
+
 	};
+
+	private void showDialog(String title) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+		builder.setTitle(title);
+		builder.show();
+	}
 }
