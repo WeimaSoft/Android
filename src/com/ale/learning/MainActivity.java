935578acee554318fe1249d41ba55fe48793417f
WeimaSoft <Alexandersu@126.com>
2016-09-12 14:06:20 +0800

    Finish OKCancel Dialog

--- src/com/ale/learning/MainActivity.java
+++ src/com/ale/learning/MainActivity.java
@@ -1,7 +1,12 @@
 package com.ale.learning;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,33 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button btnOKCancel = (Button) findViewById(R.id.btnOKCancel);
+		btnOKCancel.setOnClickListener(okCancelListner);
 	}
+
+	private OnClickListener okCancelListner = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+			builder.setTitle("你确定要离开吗？");
+			builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+
+				}
+			});
+			builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+
+				}
+			});
+			builder.create().show();
+		}
+	};
 }
