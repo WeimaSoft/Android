4c8bc0c153e55e01bbfe5844796d26df4fdf4f12
WeimaSoft <alexandersu@126.com>
2016-09-13 22:25:25 +0800

    Add Text View

--- src/com/ale/learning/CommonControlsActivity.java
+++ src/com/ale/learning/CommonControlsActivity.java
@@ -2,14 +2,28 @@ package com.ale.learning;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CommonControlsActivity extends Activity {
 
+	private static LinearLayout layout = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_common_controls);
+		
+		layout = (LinearLayout) findViewById(R.id.commonControlsLayout);
+		
+		addTextView();
+	}
+	
+	private void addTextView()
+	{
+		TextView textView = new TextView(this);
+		textView.setTextSize(22);
+		textView.setText("This is a TextView");
+		
+		layout.addView(textView);
 	}
 }
