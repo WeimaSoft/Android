0cccff697f43ac889a55eb6d995f2b8082faa3ff
WeimaSoft <Alexandersu@126.com>
2016-09-16 13:17:23 +0800

    Finish ShredPreferences

--- src/com/ale/learning/datastore/SharedPreferencesActivity.java
+++ src/com/ale/learning/datastore/SharedPreferencesActivity.java
@@ -1,5 +1,7 @@
 package com.ale.learning.datastore;
 
+import java.io.File;
+
 import com.ale.learning.R;
 
 import android.app.Activity;
@@ -20,42 +22,82 @@ public class SharedPreferencesActivity extends Activity {
 	private final static String PREFERENCES_FILE_NAME = "main.xml";
 	private final static String PREFERENCES_FILE_PATH = "data/data";
 	private final static String KEY_NAME = "name";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.datastore_sharedpreferences);
-		
+
 		initializePreferences();
-		
+
 		initializeButtonEvent();
 	}
-	
-	private void initializePreferences()
-	{
-		_preferences = getSharedPreferences(PREFERENCES_HEADER_NAME,MODE_PRIVATE);
+
+	private void initializePreferences() {
+		_preferences = getSharedPreferences(PREFERENCES_HEADER_NAME, MODE_PRIVATE);
 	}
-	
-	private void initializeButtonEvent()
-	{
+
+	private void initializeButtonEvent() {
 		Button btnSave = (Button) findViewById(R.id.btnSharedPreferencesSave);
 		btnSave.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				EditText txtInput = (EditText) findViewById(R.id.txtSharedPreferencesNameInput);
 				String inputName = txtInput.getText().toString();
-				
+
 				Editor editor = _preferences.edit();
 				editor.putString(KEY_NAME, inputName);
 				editor.commit();
-				
+
 				showDialog("Save Success!");
 			}
 		});
+
+		Button btnDisplay = (Button) findViewById(R.id.btnSharedPreferencesDisplay);
+		btnDisplay.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				EditText txtDisplay = (EditText) findViewById(R.id.lblSharedPreferencesNameReDisplay);
+				String savedName = _preferences.getString(KEY_NAME, "No any saved data");
+				
+				txtDisplay.setText(savedName);
+			}
+		});
+		
+		Button btnDeleteData = (Button) findViewById(R.id.btnSharedPreferencesDelete);
+		btnDeleteData.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Editor editor =  _preferences.edit();
+						
+				editor.clear();
+				
+				editor.commit();
+				
+				showDialog("Clear data Success!");
+			}
+		});
+		
+		Button btnDeleteFile = (Button) findViewById(R.id.btnSharedPreferencesDeleteFile);
+		btnDeleteFile.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				File file = new File(PREFERENCES_FILE_PATH + getPackageName().toString() + "shared_prefs",PREFERENCES_FILE_NAME);
+				
+				if (file.exists()) {
+					showDialog("File exist");
+					file.delete();
+					showDialog("Delete file Success!");
+				}
+			}
+		});
 	}
-	
-	private void showDialog(String mString){
+
+	private void showDialog(String mString) {
 		Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Result");
 		builder.setMessage(mString);
