fae38ef8bc86b3665e4982850262217893c35f86
WeimaSoft <Alexandersu@126.com>
2016-09-16 11:00:34 +0800

    Add button save listner

--- src/com/ale/learning/datastore/SharedPreferencesActivity.java
+++ src/com/ale/learning/datastore/SharedPreferencesActivity.java
@@ -7,6 +7,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SharedPreferencesActivity extends Activity {
 
@@ -14,5 +17,14 @@ public class SharedPreferencesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.datastore_sharedpreferences);
+		
+		Button btnSave = (Button) findViewById(R.id.btnSharedPreferencesSave);
+		btnSave.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 	}
 }
